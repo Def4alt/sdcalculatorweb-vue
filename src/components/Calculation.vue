83e90b2ebf50fcc37743a412d94eedac9899c101
Andrii Olkhovych <bronzemonet@gmail.com>
2021-02-09 21:05:25 +0200

    Change ESLint config from Airbnb to Prettier. Add Calculation component and "calculate" function in it to start with.

---
+++ src/components/Calculation.vue
@@ -0,0 +1,20 @@
+<template>
+	<div>
+		<button @click="calculate">
+			Hello
+		</button>
+	</div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from "vue-class-component";
+
+@Options({
+	methods: {
+		calculate: () => {
+			console.log("Hello");
+		},
+	},
+})
+export default class Calculation extends Vue {}
+</script>
